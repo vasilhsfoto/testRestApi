eecbba6757f21322605fc3a8d20f81e7cac065ff
vassilis <vasilhsfoto@gmail.com>
2013-06-02 14:16:17 +0300

    add test directory

---
+++ src/test/java/com/company/dao/JdbcTemplatePersonDaoTest.java
@@ -0,0 +1,14 @@
+package com.company.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JdbcTemplatePersonDaoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
